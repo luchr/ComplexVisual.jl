b54de7133d06b24aee0755897d6afe8bb09c0995
Christian Ludwig <ludwig@ma.tum.de>
2021-05-07 22:04:56 +0200

    refacotred axis

---
+++ examples/Axis01.jl
@@ -0,0 +1,69 @@
+using Cairo
+using ComplexVisual
+@ComplexVisual.import_huge
+using ComplexVisualGtk
+@ComplexVisualGtk.import_huge
+
+function axis1(layout, can_math_l, attach)
+    t1, t2 = collect(-2:2), collect(-1.5:1.0:1.5)
+    t3 = setdiff(setdiff(collect(-2:0.1:2), t1), t2)
+
+    ticks = cv_format_ticks("%.0f", t1...)
+    ticksh = cv_format_ticks("%.1f", t2...)
+    tickss = cv_format_ticks("", t3...)
+
+    app = CV_TickLabelAppearance(;
+        tick_length=10,
+        label_style=cv_color(0,0,0) → 
+            cv_fontface("DejaVu Sans", Cairo.FONT_WEIGHT_BOLD) → 
+            cv_fontsize(20))
+    apph = CV_TickLabelAppearance(; tick_length=6,
+        label_style=cv_color(0,0,0) → 
+            cv_fontface("DejaVu Sans") → cv_fontsize(10))
+    apps = CV_TickLabelAppearance(; tick_length=3)
+    return cv_ticks_labels(layout, can_math_l, attach,
+        (CV_Ruler(ticks, app), CV_Ruler(ticksh, apph), CV_Ruler(tickss, apps)))
+end
+
+function main()
+    slider_pos = CV_TranslateByOffset(Float64)
+    slider_pos.value = 40.0
+
+    layout = CV_2DLayout()
+
+    can_math = CV_Math2DCanvas(-2.0 + 2.0im, 2.0 - 2.0im, 100)
+    can_math_l = cv_add_canvas!(layout, can_math,
+        cv_anchor(can_math, :center), (0,0))
+    cc_can_math = cv_create_context(can_math)
+
+    a1_l = axis1(layout, can_math_l, cv_south)
+    a2_l = axis1(layout, can_math_l, cv_north)
+    a3_l = axis1(layout, can_math_l, cv_east)
+    a4_l = axis1(layout, can_math_l, cv_west)
+
+    cv_add_padding!(layout, 30)
+
+    fill_red = cv_color(1,0,0) ↦ CV_2DCanvasFillPainter()
+
+    can_layout = cv_canvas_for_layout(layout)
+    cc_can_layout = cv_create_context(can_layout)
+    ec = CV_EmptyPaintingContext()
+
+    update_math_domains = z -> begin
+        cv_paint(cc_can_math, fill_red, ec)
+        can_math_l(cc_can_layout)
+        a1_l(cc_can_layout)
+        a2_l(cc_can_layout)
+        a3_l(cc_can_layout)
+        a4_l(cc_can_layout)
+    end
+    update_math_domains(0.0+0.0im)
+
+    handler = cvg_handler_for_canvas(can_layout, "Slider01")
+
+    cvg_wait_for_destroy(handler.window)
+end
+
+main()
+
+# vim:syn=julia:cc=79:fdm=marker:sw=4:ts=4:
--- src/axis.jl
+++ src/axis.jl
@@ -1,7 +1,7 @@
 macro import_axis_huge()
     :(
         using ComplexVisual:
-            CV_TickLabel, cv_format_ticks, cv_create_context,
+            CV_TickLabel, cv_format_ticks,
             CV_TickLabelAppearance, CV_Ruler,
             cv_create_2daxis_canvas, cv_ticks_labels, cv_anchor
     )
@@ -12,7 +12,7 @@ import Base: show
 
 """
 Location (math coordinate) and description for a tick
-(typically placed at axis).
+(typically placed at axis at a axis-tick).
 """
 struct CV_TickLabel{LocT}
     location    :: LocT
@@ -23,23 +23,20 @@ end
 Styles and data needed to render/draw axis-ticks with labels.
 """
 struct CV_TickLabelAppearance{tsT <: CV_ContextStyle, lsT <: CV_ContextStyle} # {{{
-    attach       :: CV_AttachType
     tick_length  :: Int32
     gap          :: Int32
     tick_style   :: tsT
     label_style  :: lsT
 
-    function CV_TickLabelAppearance(attach::CV_AttachType,
-            tick_length::Integer, gap::Integer, tick_style::CV_ContextStyle,
-            label_style::CV_ContextStyle)
+    function CV_TickLabelAppearance(tick_length::Integer, gap::Integer,
+        tick_style::CV_ContextStyle, label_style::CV_ContextStyle)
         return new{typeof(tick_style), typeof(label_style)}(
-            attach, Int32(tick_length), Int32(gap), tick_style, label_style)
+            Int32(tick_length), Int32(gap), tick_style, label_style)
     end
 end
 
 function show(io::IO, app::CV_TickLabelAppearance)
-    print(io, "CV_TickLabelAppearance(attach: "); show(io, app.attach)
-    print(io, ", tick_length: "); show(io, app.tick_length)
+    print(io, "CV_TickLabelAppearance(tick_length: "); show(io, app.tick_length)
     print(io, ", gap: "); show(io, app.gap)
     print(io, ", tick_style: "); show(io, app.tick_style)
     print(io, ", label_style: "); show(io, app.label_style)
@@ -52,7 +49,6 @@ function show(io::IO, m::MIME{Symbol("text/plain")}, app::CV_TickLabelAppearance
     indent = outer_indent * "  "
     iio = IOContext(io, :cv_indent => indent)
     println(io, "CV_TickLabelAppearance(")
-    print(io, indent, "attach: "); show(iio, app.attach); println(io)
     print(io, indent, "tick_length: "); show(iio, app.tick_length); println(io)
     print(io, indent, "gap: "); show(iio, app.gap); println(io)
     print(io, indent, "tick_style: "); show(iio, m, app.tick_style); println(io)
@@ -62,13 +58,12 @@ function show(io::IO, m::MIME{Symbol("text/plain")}, app::CV_TickLabelAppearance
 end # }}}
 
 function CV_TickLabelAppearance(;
-        attach::CV_AttachType=cv_south,
         tick_length::Integer=Int32(10),
         gap::Integer=cv_half(tick_length),
         tick_style::CV_ContextStyle=cv_linewidth(2) → cv_color(0,0,0),
         label_style::CV_ContextStyle=cv_color(0,0,0) →
             cv_fontface("serif") → cv_fontsize(15))
-    return CV_TickLabelAppearance(attach, tick_length, gap, tick_style, label_style)
+    return CV_TickLabelAppearance(tick_length, gap, tick_style, label_style)
 end
 
 """
@@ -110,7 +105,8 @@ with the given printf-format.
 """
 function cv_format_ticks(printf_format::AbstractString, 
         locations::Vararg{Real, N}) where {N}
-    @eval fmt_func = x -> @sprintf($printf_format, x)
+    fmt_func = isempty(printf_format) ?
+        x -> "" : @eval x -> @sprintf($printf_format, x)
     return NTuple{N, CV_TickLabel{Float64}}(
         map(x -> CV_TickLabel{Float64}(
                 Float64(x), Base.invokelatest(fmt_func, Float64(x))),
@@ -126,7 +122,43 @@ function cv_format_ticks(locations::Vararg{Real, N}) where {N}
 end
 
 """
-struct used internally for constructing the ticks, labels and axis canvas.
+struct used internally for constructing the ticks and labels.
+
+Needed in advance (before allocating the axis canvas) in order to
+be able to determine the size needed for the axis canvas.
+
+```
+                     ┌─────── cv_north ────────┐
+                     │ label                   │
+                     │                         │               ┌─────────┐
+                     │   │                     │               │         │
+              ┌──────┴─*─┴─────────────────────┴───────────────*         c
+              │        │                                       │         v
+              │        │                                       │         _
+              c        │                                       ├── lab   e
+              v lab1 ──┤                                       │         a
+              _        │                                       │         s
+              w        │                                       │         t
+              e        │      math canvas                      │         │
+              s        │                                       │         │
+              t lab2 ──┤                                       ├─────────┘
+              │        │                                       │
+              │        │                                       │
+              │        │                                       │
+              └────────┤                                       │
+                       │                                       │
+                       │                                       │
+                       │                                       │
+                  ┌────*──┬───────────────┬───────────────┬────┘
+                  │       │tick_length    │               │
+                  │       │               │               │
+                  │         gap                           │
+                  │                                       │
+                  │  tick label         label2            │
+                  └─────────────── cv_south ──────────────┘
+
+         *   is (0,0) for axis canvas units (and :default-anchor from outside)
+```
 """
 mutable struct CV_TickLabelData{LocT}
     text           :: AbstractString
@@ -146,61 +178,60 @@ struct CV_TicksLabelsMetric
     max_width  :: Int32
 end
 
-function cv_ticks_labels_draw(canvas::CV_2DCanvas,
+function cv_ticks_labels_draw(con::CV_2DCanvasContext,
         attach::CV_AttachType, app::CV_TickLabelAppearance,
         ticklabelsdata::NTuple{N, CV_TickLabelData{Float64}};
         debug::Union{Val{true}, Val{false}}=Val(false)) where {N}  # {{{
 
     tick_length, tick_style = app.tick_length, app.tick_style
     label_style = app.label_style
+    canvas = con.canvas
 
-    cv_create_context(canvas) do con
-        ctx = con.ctx
-        ubox = canvas.user_box
-        # ticks
-        cv_prepare(con, tick_style)
+    ctx = con.ctx
+    ubox = canvas.user_box
+    # ticks
+    cv_prepare(con, tick_style)
+    for td in ticklabelsdata
+        if attach isa CV_southT
+            move_to(ctx, td.tick_location, 0)
+            rel_line_to(ctx, 0, tick_length)
+        elseif attach isa CV_northT
+            move_to(ctx, td.tick_location, 0)
+            rel_line_to(ctx, 0, -tick_length)
+        elseif attach isa CV_eastT
+            move_to(ctx, 0, td.tick_location)
+            rel_line_to(ctx, tick_length, 0)
+        elseif attach isa CV_westT
+            move_to(ctx, 0, td.tick_location)
+            rel_line_to(ctx, -tick_length, 0)
+        else
+            cv_error("Unknown attach value")
+        end
+        stroke(ctx)
+    end
+
+    # labels
+    cv_prepare(con, label_style)
+    for td in ticklabelsdata
+        if !td.bounding_box.empty
+            move_to(ctx, td.startpoint...)
+            show_text(ctx, td.text)
+        end
+    end
+
+    # bb check
+    if debug isa Val{true}
+        set_line_width(ctx, 1)
+        set_source_rgb(ctx, 1, 0, 0)
         for td in ticklabelsdata
-            if attach isa CV_southT
-                move_to(ctx, td.tick_location, 0)
-                rel_line_to(ctx, 0, tick_length)
-            elseif attach isa CV_northT
-                move_to(ctx, td.tick_location, ubox.top)
-                rel_line_to(ctx, 0, -tick_length)
-            elseif attach isa CV_eastT
-                move_to(ctx, 0, td.tick_location)
-                rel_line_to(ctx, tick_length, 0)
-            elseif attach isa CV_westT
-                move_to(ctx, ubox.right, td.tick_location)
-                rel_line_to(ctx, -tick_length, 0)
-            else
-                cv_error("Unknown attach value")
-            end
+            bb = td.bounding_box
+            rectangle(ctx, bb.left, bb.bottom, cv_width(bb), cv_height(bb))
             stroke(ctx)
         end
 
-        # labels
-        cv_prepare(con, label_style)
-        for td in ticklabelsdata
-            if !td.bounding_box.empty
-                move_to(ctx, td.startpoint...)
-                show_text(ctx, td.text)
-            end
-        end
-
-        # bb check
-        if debug isa Val{true}
-            set_line_width(ctx, 1)
-            set_source_rgb(ctx, 1, 0, 0)
-            for td in ticklabelsdata
-                bb = td.bounding_box
-                rectangle(ctx, bb.left, bb.bottom, cv_width(bb), cv_height(bb))
-                stroke(ctx)
-            end
-
-            set_source_rgb(ctx, 0, 1, 0)
-            rectangle(ctx, ubox.left, ubox.bottom, cv_width(ubox), cv_height(ubox))
-            stroke(ctx)
-        end
+        set_source_rgb(ctx, 0, 1, 0)
+        rectangle(ctx, ubox.left, ubox.bottom, cv_width(ubox), cv_height(ubox))
+        stroke(ctx)
     end
 end # }}}
 
@@ -241,21 +272,18 @@ end # }}}
 function cv_ticks_labels_base(tick_length, gap, tlm, attach) # {{{
     return (
         attach isa CV_southT ? tick_length + gap + tlm.max_height  :
-        attach isa CV_northT ? tlm.max_height                      :
+        attach isa CV_northT ? -tick_length - gap - tlm.max_depth  :
         attach isa CV_eastT  ? tick_length + gap                   :
-        attach isa CV_westT  ? tlm.max_width                       :
+        attach isa CV_westT  ? -tick_length - gap                  :
         cv_error("Unknown attach value")) :: Int32
 end # }}}
 
 function cv_set_tick_location!(ticklabelsdata, for_canvas, attach) # {{{
     for td in ticklabelsdata
-        if (attach isa CV_northT) || (attach isa CV_southT)
-            td.tick_location = cv_math2pixel(
-                for_canvas, td.math_location, 0.0)[1]
-        else
-            td.tick_location = cv_math2pixel(
-                for_canvas, 0.0, td.math_location)[2]
-        end
+        td.tick_location = (
+            ((attach isa CV_northT) || (attach isa CV_southT))    ?
+            cv_math2pixel(for_canvas, td.math_location, 0.0)[1]   :
+            cv_math2pixel(for_canvas, 0.0, td.math_location)[2])
     end
     return nothing
 end # }}}
@@ -294,117 +322,130 @@ function cv_add_rect_for_ticks(rstore, ticklabelsdata, tick_length,
     max_loc = ceil(Int32, maximum(map(td -> td.tick_location, ticklabelsdata)))
     min_loc = floor(Int32, minimum(map(td -> td.tick_location, ticklabelsdata)))
     z, o = zero(Int32), one(Int32)
-    if (min_loc == max_loc)
+    if min_loc == max_loc
         max_loc += o
     end
-    cur_top, cur_right = rstore.bounding_box.top, rstore.bounding_box.right
-    new_top = cur_top + tick_length + gap
-    new_right = cur_right + tick_length + gap
     cv_add_rectangle!(rstore, (
-        attach isa CV_southT ? CV_Rectangle(tick_length, min_loc, z, max_loc) :
-        attach isa CV_northT ? CV_Rectangle(new_top, min_loc,
-                                            new_top-o, max_loc)               :
-        attach isa CV_eastT  ? CV_Rectangle(max_loc, z, min_loc, tick_length) :
-        attach isa CV_westT  ? CV_Rectangle(max_loc, new_right-o,
-                                            min_loc, new_right)               :
+        attach isa CV_southT ? CV_Rectangle(tick_length, min_loc, z, max_loc)  :
+        attach isa CV_northT ? CV_Rectangle(z, min_loc, -tick_length, max_loc) :
+        attach isa CV_eastT  ? CV_Rectangle(max_loc, z, min_loc, tick_length)  :
+        attach isa CV_westT  ? CV_Rectangle(max_loc, -tick_length, min_loc, z) :
         cv_error("Unknown attach value")) :: CV_Rectangle{Int32})
     return nothing
 end # }}}
 
 function cv_add_rect_for_default_anchor(rstore, attach) # {{{
     z, o = zero(Int32), one(Int32)
-    cur_top, cur_right = rstore.bounding_box.top, rstore.bounding_box.right
     cv_add_rectangle!(rstore, (
-        attach isa CV_southT ? CV_Rectangle(o, z, z, o)                   :
-        attach isa CV_northT ? CV_Rectangle(cur_top, z, cur_top-o, o)     :
-        attach isa CV_eastT  ? CV_Rectangle(o, z, z, o)                   :
-        attach isa CV_westT  ? CV_Rectangle(o, cur_right-o, z, cur_right) :
+        attach isa CV_southT ? CV_Rectangle(o, z, z, o)      :
+        attach isa CV_northT ? CV_Rectangle(z, z, -o, o)     :
+        attach isa CV_eastT  ? CV_Rectangle(o, z, z, o)      :
+        attach isa CV_westT  ? CV_Rectangle(o, -o, z, z)     :
         cv_error("Unknown attach value")) :: CV_Rectangle{Int32})
     return nothing
 end # }}}
-
 # }}}
 
 function cv_get_default_anchor(rstore, attach)  # {{{
     z, o = zero(Int32), one(Int32)
     bb = rstore.bounding_box
     return (
-        attach isa CV_southT ? (-bb.left, z)             :
-        attach isa CV_northT ? (-bb.left, bb.top)        :
-        attach isa CV_eastT  ? (z, -bb.bottom)           :
-        attach isa CV_westT  ? (bb.right, -bb.bottom)    :
+        attach isa CV_southT ? (-bb.left, z)              :
+        attach isa CV_northT ? (-bb.left, cv_height(bb))  :
+        attach isa CV_eastT  ? (z, -bb.bottom)            :
+        attach isa CV_westT  ? (cv_width(bb), -bb.bottom) :
         cv_error("Unknown attach value")) :: Tuple{Int32, Int32}
+
 end # }}}
 
-
-function cv_create_2daxis_canvas(for_canvas::CV_Math2DCanvas,
-        ruler::CV_Ruler{N, Float64, tsT, lsT}) where {N, tsT, lsT}
-    return cv_create_2daxis_canvas(
-        for_canvas, ruler.ticklabels...; app=ruler.app)
-end
-
-function cv_create_2daxis_canvas(for_canvas::CV_Math2DCanvas,
-        ticklabels::Vararg{CV_TickLabel{Float64}, N};
-        app::CV_TickLabelAppearance=CV_TickLabelAppearance()) where {N}
-
+function cv_layout_ruler(for_canvas::CV_Math2DCanvas, attach::CV_AttachType,
+        ruler::CV_Ruler, rstore::CV_RectangleStore{Int32})  # {{{
+    app = ruler.app
     tick_length, tick_style = app.tick_length, app.tick_style
-    gap, label_style, attach = app.gap, app.label_style, app.attach
+    gap, label_style = app.gap, app.label_style
 
-    ticklabelsdata = cv_init_ticklables_data(label_style, ticklabels...)
+    ticklabelsdata = cv_init_ticklables_data(label_style, ruler.ticklabels...)
     tlm = cv_ticks_labels_get_metric(ticklabelsdata)
 
     base = cv_ticks_labels_base(tick_length, gap, tlm, attach)
     cv_set_tick_location!(ticklabelsdata, for_canvas, attach)
 
-    rstore = CV_RectangleStore(Int32)     # all bounding boxes of the labels
     for td in ticklabelsdata
         xpos = cv_get_label_xpos(td, base, attach)
         ypos = cv_get_label_ypos(td, base, attach)
         td.bounding_box = cv_get_label_bb(td, xpos, ypos)
-        td.startpoint = (xpos - round(Int32, td.text_extents.x_bearing),
-                         ypos)
-        if !td.bounding_box.empty
-            cv_add_rectangle!(rstore, td.bounding_box)
-        end
+        td.startpoint = (xpos - round(Int32, td.text_extents.x_bearing), ypos)
+        !td.bounding_box.empty && cv_add_rectangle!(rstore, td.bounding_box)
     end
-
     cv_add_rect_for_ticks(rstore, ticklabelsdata, tick_length, gap, attach)
     cv_add_rect_for_default_anchor(rstore, attach)
 
-    default_anchor = cv_get_default_anchor(rstore, attach)
+    return ticklabelsdata
+end # }}}
 
+
+function cv_create_2daxis_canvas(for_canvas::CV_Math2DCanvas,
+        attach::CV_AttachType,
+        ticklabels::Vararg{CV_TickLabel{Float64}, N};
+        app::CV_TickLabelAppearance=CV_TickLabelAppearance()) where {N}
+    return cv_create_2daxis_canvas(for_canvas,
+        attach, (CV_Ruler(ticklabels, app),))
+end
+
+function cv_create_2daxis_canvas(for_canvas::CV_Math2DCanvas,
+        attach::CV_AttachType,
+        rulers::NTuple{N, CV_Ruler}) where {N} # {{{
+
+    N < 1 && cv_error("Need at least one ruler")
+    rstore = CV_RectangleStore(Int32)     # all bounding boxes of the labels
+
+    ticklabelsdata = tuple(
+        map(ruler -> cv_layout_ruler(
+            for_canvas, attach, ruler, rstore), rulers)...)
+
+    default_anchor = cv_get_default_anchor(rstore, attach)
     anchor_func = (can, anchor_name) -> (anchor_name === :default ?
             default_anchor : cv_anchor(can.bounding_box, anchor_name))
-
     canvas = CV_2DLayoutCanvas(rstore.bounding_box, anchor_func)
 
-    cv_ticks_labels_draw(canvas, attach, app, ticklabelsdata)
+    cv_create_context(canvas) do con
+        set_operator(con.ctx, Cairo.OPERATOR_SOURCE)
+        set_source_rgba(con.ctx, 0, 0, 0, 0)
+        ubox = canvas.user_box
+        rectangle(con.ctx, ubox.left, ubox.bottom, cv_width(ubox), cv_height(ubox))
+        fill(con.ctx)
+        set_operator(con.ctx, Cairo.OPERATOR_OVER)
+
+        for index in 1:N
+            cv_ticks_labels_draw(con,
+                attach, rulers[index].app, ticklabelsdata[index])
+        end
+    end
+
     return canvas
-end
+end # }}}
 
-function cv_ticks_labels(
-        layout::CV_Abstract2DLayout,
+
+function cv_ticks_labels(layout::CV_Abstract2DLayout,
         for_canvas_l::CV_2DLayoutPosition{CV_Math2DCanvas, dcbT, styleT},
-        ruler::CV_Ruler{N, Float64, tsT, lsT}) where {dcbT, styleT, N,
-                                                      tsT, lsT}
-    return cv_ticks_labels(
-        layout, for_canvas_l, ruler.ticklabels...; app=rulers.app)
-end
-
-function cv_ticks_labels(
-        layout::CV_Abstract2DLayout,
-        for_canvas_l::CV_2DLayoutPosition{CV_Math2DCanvas, dcbT, styleT},
-        ticklabels::Vararg{CV_TickLabel{Float64}, N};
-        app::CV_TickLabelAppearance=CV_TickLabelAppearance()) where {N, dcbT, styleT}
-
-    canvas = cv_create_2daxis_canvas(for_canvas_l.canvas, ticklabels...; app)
-    attach = app.attach
+        attach::CV_AttachType,
+        rulers::NTuple{N, CV_Ruler}) where {dcbT, styleT, N}
+    canvas = cv_create_2daxis_canvas(for_canvas_l.canvas, attach, rulers)
 
     return cv_add_canvas!(layout, canvas, cv_anchor(canvas, :default), 
         cv_anchor(for_canvas_l, (
             attach isa CV_southT ? :southwest   :
             attach isa CV_northT ? :northwest   :
             attach isa CV_eastT  ? :northeast   : :northwest)))
+end
+
+function cv_ticks_labels(layout::CV_Abstract2DLayout,
+        for_canvas_l::CV_2DLayoutPosition, attach::CV_AttachType,
+        ticklabels::Vararg{CV_TickLabel{Float64}, N};
+        app::CV_TickLabelAppearance=CV_TickLabelAppearance()) where {N}
+
+    return cv_ticks_labels(layout, for_canvas_l,
+        attach, (CV_Ruler(ticklabels, app), ))
 end
 
 
