d4a6eece45a5a1f591b739890a491148a395d131
Christian Ludwig <ludwig@ma.tum.de>
2021-05-25 14:32:24 +0200

    docu: tutorial helper: fadeout alpha mask

--- docs/TutorialHelpers.jl
+++ docs/TutorialHelpers.jl
@@ -114,5 +114,44 @@ function substitute_marker_in_markdown(context::SubstMDcontext, md)
     return nothing
 end
 
+
+"""
+creates a surface (width `w` and height `h`) where the alpha component
+is decreased (to 0) near the boundary.
+"""
+function create_fadeout_surface(w::Int32, h::Int32, dist::Int32=30)
+    if 2*dist > w || 2*dist > h
+        cv_error("d too large: 2*dist > w  or 2*dist > h")
+    end
+    canvas = CV_Std2DCanvas(w, h)
+    Cairo.flush(canvas.surface)
+    data = canvas.surface.data
+    for x = 1:dist
+        p1 = (dist-x)^2
+        data[x    , dist+1:h-dist] .= p1
+        data[w-x+1, dist+1:h-dist] .= p1
+        for y = 1:dist
+            p2 = (dist-y)^2
+            data[x, y] = data[x, h-y+1] = 
+            data[w-x+1, y] = data[w-x+1, h-y+1] = UInt32(p1 + p2)
+        end
+    end
+    for y = 1:dist
+        p2 = (dist-y)^2
+        data[dist+1:w-dist, y] .= p2
+        data[dist+1:w-dist, h-y+1] .= p2
+    end
+    data[dist+1:w-dist, dist+1:h-dist] .= 0x0
+
+    # normalize
+    data[:] .= round.(UInt32, 
+        255*(
+            1.0 .- max.(min.(Float64.(data[:])/((dist-3)^2), 1.0), 0.0)
+            )) .<< 24
+ 
+    Cairo.mark_dirty(canvas.surface)
+    return canvas
+end
+
 # vim:syn=julia:cc=79:fdm=marker:sw=4:ts=4:
 
